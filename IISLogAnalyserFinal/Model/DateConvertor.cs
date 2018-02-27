fa5c4ef34e0b4d2ff2cfaafe750e0da4a6a56bf7
Ivailo Ivanov <ivailo.ivanov89@gmail.com>
2018-02-27 13:53:47 +0200

    Test commit

--- IISLogAnalyserFinal/Model/DateConvertor.cs
+++ IISLogAnalyserFinal/Model/DateConvertor.cs
@@ -8,6 +8,6 @@ namespace IISLogAnalyserFinal.Model
 {
     public class DateConvertor : IFieldConvertor
     {
-        public dynamic Convert(string text) => DateTimeOffset.ParseExact(text, "yyyy'-'MM'-'dd", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal);
+        public dynamic Convert(string text) => DateTimeOffset.ParseExact(text, "yyyy'-'MM'-'dd", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal); 
     }
 }
